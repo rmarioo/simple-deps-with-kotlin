9114c1843714ff31e66bf161e3e84fe5348f1abc
Mario Russo <mario.russo@lastminute.com>
2018-12-27 21:30:55 +0100

    public function is on the top of mainApi

--- src/main/kotlin/reader/api/MainApi.kt
+++ src/main/kotlin/reader/api/MainApi.kt
@@ -7,10 +7,11 @@ import reader.model.Registration
 
 interface MainApi: AuthApi, CourseApi {
 
-    fun Person.applyTo(course: Course): Option<Registration> =
-                           authorize()
+    fun registerToCourse(person: Person, course: Course) = person.applyTo(course)
+
+    private fun Person.applyTo(course: Course): Option<Registration> =
+            authorize()
                     .map { user -> user.registerToCourse(course) }
 
-    fun registerToCourse(person: Person, course: Course) = person.applyTo(course)
 
 }
